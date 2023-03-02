169e7a3e680d8801bde5bba166186712dd753ade
megane <susini.megane04@gmail.com>
2023-03-02 10:04:22 +0100

    add ranking + ranking v1 completed

--- model/functions.php
+++ model/functions.php
@@ -1,23 +1,218 @@
 <?php
-function generateNb() 
-{
-return mt_rand(0,100);
-}
+    require("../model/BdPdoConnection.php");
+    require("../model/PlayerDAO.php");
+    require("../model/GameDAO.php");
+    require("../model/RoundDAO.php");
+    require("../model/Round1DAO.php");
+    require("../model/Round2DAO.php");
+    require("../model/Round3DAO.php");      
 
-function findTheNb($userNb, $nbToFind)
-{
-    if ($userNb == $nbToFind) 
+    $newPlayerDAO = new PlayerDAO(BdPdoConnection::getConnection());
+    $newGameDAO = new GameDAO(BdPdoConnection::getConnection());
+    $newRoundDAO = new RoundDAO(BdPdoConnection::getConnection());
+    $newRound1DAO = new Round1DAO(BdPdoConnection::getConnection());
+    $newRound2DAO = new Round2DAO(BdPdoConnection::getConnection());
+    $newRound3DAO = new Round3DAO(BdPdoConnection::getConnection());
+
+    // FUNCTIONS //
+
+    // the best players of the game
+    function displayBestPlayers()
     {
-        return "You are right ! The number is ".$nbToFind;
+        global $newPlayerDAO, $newGameDAO, $newRoundDAO, $newRound1DAO, $newRound2DAO, $newRound3DAO;
+
+        // get the players
+        $players = $newPlayerDAO->getAllPlayers(); 
+
+        $tableHtml = "<table class='bestPlayersRankTable'><thead><tr>";
+        $headers = ["TOP", "USER NAME", "TOTAL TIME", "TOTAL POINTS /32"];
+
+        // add the headers
+        for ($i = 0; $i < count($headers); $i++) {
+            $tableHtml .= "<th>" . strval($headers[$i]) . "</th>";
+        }
+
+        $tableHtml .= "</tr></thead><tbody>";
+
+        for ($i = 0; $i < count($players); $i++) {
+            $tableHtml .= "<tr><td>" . strval($i+1) . "</td>"; // top
+            $tableHtml .= "<td>" . $players[$i]["pseudo"] . "</td>"; // user name
+            // total time
+            $gameId = $newGameDAO->selectGame($players[$i]["id"])[0]["MAX(id)"];
+            $roundId = $newRoundDAO->selectRound($gameId)[0]["id"];
+            $round1Time = $newRound1DAO->selectRound1($roundId)[0]["timeSpent"];
+            $round2Time = $newRound2DAO->selectRound2($roundId)[0]["timeSpent"];
+            $round3Time = $newRound3DAO->selectRound3($roundId)[0]["timeSpent"];
+            $totalTime = $round1Time + $round2Time + $round3Time;
+
+            $tableHtml .= "<td>" . strval($totalTime) . " seconds</td>";
+
+            // total points
+            $round1Tries = $newRound1DAO->selectRound1($roundId)[0]["triesNb"]; // /15
+            $round2WordsNb = $newRound2DAO->selectRound2($roundId)[0]["wordsNb"]; // /14
+            $round3RightAnswers = $newRound3DAO->selectRound3($roundId)[0]["rightAnswersNb"]; // /3
+            if ($round1Tries > 15)
+            {
+                $round1Tries = 15;
+            }
+            $totalPoints = $round2WordsNb + $round3RightAnswers + (15 - $round1Tries);
+
+            $tableHtml .= "<td>" . strval($totalPoints) . " points</td></tr>";
+        }
+        $tableHtml .= "</tbody></table>";
+        echo $tableHtml;
     }
-    else if ($userNb > $nbToFind)
+
+    // round1 ranking
+    function displayRound1Rank()
     {
-        return "less";
+        global $newPlayerDAO, $newGameDAO, $newRoundDAO, $newRound1DAO, $newRound2DAO, $newRound3DAO;
+
+        // get the players
+        $players = $newPlayerDAO->getAllPlayers(); 
+
+        $tableHtml = "<table class='roundsRankTable'><thead><tr>";
+        $headers = ["TOP", "USER NAME", "TIME", "TRIES"];
+
+        // add the headers
+        for ($i = 0; $i < count($headers); $i++) {
+            $tableHtml .= "<th>" . strval($headers[$i]) . "</th>";
+        }
+
+        $tableHtml .= "</tr></thead><tbody>";
+
+        for ($i = 0; $i < count($players); $i++) {
+            $tableHtml .= "<tr><td>" . strval($i+1) . "</td>"; // top
+            $tableHtml .= "<td>" . $players[$i]["pseudo"] . "</td>"; // user name
+            $gameId = $newGameDAO->selectGame($players[$i]["id"])[0]["MAX(id)"];
+            $roundId = $newRoundDAO->selectRound($gameId)[0]["id"];
+            $round1Time = $newRound1DAO->selectRound1($roundId)[0]["timeSpent"];
+            $tableHtml .= "<td>" . strval($round1Time) . " seconds</td>"; // round1 time
+            $round1Tries = $newRound1DAO->selectRound1($roundId)[0]["triesNb"]; // /15
+            $tableHtml .= "<td>" . strval($round1Tries) . " tries</td></tr>"; // round1 tries
+        }
+        $tableHtml .= "</tbody></table>";
+        echo $tableHtml;
     }
-    else
+
+    // round2 ranking
+    function displayRound2Rank()
     {
-        return "more";
+        global $newPlayerDAO, $newGameDAO, $newRoundDAO, $newRound1DAO, $newRound2DAO, $newRound3DAO;
+
+        // get the players
+        $players = $newPlayerDAO->getAllPlayers(); 
+
+        $tableHtml = "<table class='roundsRankTable'><thead><tr>";
+        $headers = ["TOP", "USER NAME", "TIME", "WORDS FOUND /14"];
+
+        // add the headers
+        for ($i = 0; $i < count($headers); $i++) {
+            $tableHtml .= "<th>" . strval($headers[$i]) . "</th>";
+        }
+
+        $tableHtml .= "</tr></thead><tbody>";
+
+        for ($i = 0; $i < count($players); $i++) {
+            $tableHtml .= "<tr><td>" . strval($i+1) . "</td>"; // top
+            $tableHtml .= "<td>" . $players[$i]["pseudo"] . "</td>"; // user name
+            $gameId = $newGameDAO->selectGame($players[$i]["id"])[0]["MAX(id)"];
+            $roundId = $newRoundDAO->selectRound($gameId)[0]["id"];
+            $round2Time = $newRound2DAO->selectRound2($roundId)[0]["timeSpent"];
+            $tableHtml .= "<td>" . strval($round2Time) . " seconds</td>"; // round2 time
+            $round2WordsNb = $newRound2DAO->selectRound2($roundId)[0]["wordsNb"]; // /14
+            $tableHtml .= "<td>" . strval($round2WordsNb) . " word(s) found</td></tr>"; // round2 words found
+        }
+        $tableHtml .= "</tbody></table>";
+        echo $tableHtml;
     }
-}
+
+    // round3 ranking
+    function displayRound3Rank()
+    {
+        global $newPlayerDAO, $newGameDAO, $newRoundDAO, $newRound1DAO, $newRound2DAO, $newRound3DAO;
+
+        // get the players
+        $players = $newPlayerDAO->getAllPlayers(); 
+
+        $tableHtml = "<table class='roundsRankTable'><thead><tr>";
+        $headers = ["TOP", "USER NAME", "TIME", "RIGHT ANSWERS /3"];
+
+        // add the headers
+        for ($i = 0; $i < count($headers); $i++) {
+            $tableHtml .= "<th>" . strval($headers[$i]) . "</th>";
+        }
+
+        $tableHtml .= "</tr></thead><tbody>";
+
+        for ($i = 0; $i < count($players); $i++) {
+            $tableHtml .= "<tr><td>" . strval($i+1) . "</td>"; // top
+            $tableHtml .= "<td>" . $players[$i]["pseudo"] . "</td>"; // user name
+            $gameId = $newGameDAO->selectGame($players[$i]["id"])[0]["MAX(id)"];
+            $roundId = $newRoundDAO->selectRound($gameId)[0]["id"];
+            $round3Time = $newRound3DAO->selectRound3($roundId)[0]["timeSpent"];
+            $tableHtml .= "<td>" . strval($round3Time) . " seconds</td>"; // round3 time
+            $round3RightAnswers = $newRound3DAO->selectRound3($roundId)[0]["rightAnswersNb"]; // /3
+            $tableHtml .= "<td>" . strval($round3RightAnswers) . " right answer(s)</td></tr>"; // round3 right answers
+        }
+        $tableHtml .= "</tbody></table>";
+        echo $tableHtml;
+    }
+
+    // global array
+    function displayRankTable()
+    {
+        global $newPlayerDAO, $newGameDAO, $newRoundDAO, $newRound1DAO, $newRound2DAO, $newRound3DAO;
+
+        // get the players
+        $players = $newPlayerDAO->getAllPlayers(); 
+
+        $tableHtml = "<table class='rankTable'><thead><tr>";
+        $headers = ["TOP", "USER NAME", "TOTAL TIME", "TOTAL POINTS /32", "ROUND1 : TIME", "ROUND1 : TRIES", "ROUND2 : TIME", "ROUND2 : WORDS FOUND /14", "ROUND3 : TIME", "ROUND3 : RIGHT ANSWERS /3"];
+
+        // add the headers
+        for ($i = 0; $i < count($headers); $i++) {
+            $tableHtml .= "<th>" . strval($headers[$i]) . "</th>";
+        }
+
+        $tableHtml .= "</tr></thead><tbody>";
+
+        for ($i = 0; $i < count($players); $i++) {
+            $tableHtml .= "<tr><td>" . strval($i+1) . "</td>"; // top
+            $tableHtml .= "<td>" . $players[$i]["pseudo"] . "</td>"; // user name
+            
+            // total time
+            $gameId = $newGameDAO->selectGame($players[$i]["id"])[0]["MAX(id)"];
+            $roundId = $newRoundDAO->selectRound($gameId)[0]["id"];
+            $round1Time = $newRound1DAO->selectRound1($roundId)[0]["timeSpent"];
+            $round2Time = $newRound2DAO->selectRound2($roundId)[0]["timeSpent"];
+            $round3Time = $newRound3DAO->selectRound3($roundId)[0]["timeSpent"];
+            $totalTime = $round1Time + $round2Time + $round3Time;
+
+            $tableHtml .= "<td>" . strval($totalTime) . " seconds</td>";
+
+            // total points
+            $round1Tries = $newRound1DAO->selectRound1($roundId)[0]["triesNb"]; // /15
+            $round2WordsNb = $newRound2DAO->selectRound2($roundId)[0]["wordsNb"]; // /14
+            $round3RightAnswers = $newRound3DAO->selectRound3($roundId)[0]["rightAnswersNb"]; // /3
+            if ($round1Tries > 15)
+            {
+                $round1Tries = 15;
+            }
+            $totalPoints = $round2WordsNb + $round3RightAnswers + (15 - $round1Tries);
+
+            $tableHtml .= "<td>" . strval($totalPoints) . " points</td>";
+
+            $tableHtml .= "<td>" . strval($round1Time) . " seconds</td>"; // round1 time
+            $tableHtml .= "<td>" . strval($round1Tries) . " tries</td>"; // round1 tries
+            $tableHtml .= "<td>" . strval($round2Time) . " seconds</td>"; // round2 time
+            $tableHtml .= "<td>" . strval($round2WordsNb) . " word(s) found</td>"; // round2 words found
+            $tableHtml .= "<td>" . strval($round3Time) . " seconds</td>"; // round3 time
+            $tableHtml .= "<td>" . strval($round3RightAnswers) . " right answer(s)</td></tr>"; // round3 right answers
+
+        }
+        $tableHtml .= "</tbody></table>";
+        echo $tableHtml;
+    }
 
 ?>
--- view/ranking.php
+++ view/ranking.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+require("../model/functions.php");
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -8,85 +9,30 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ranking of the best players</title>
+    <link rel="stylesheet" href="../css/styles.css">
 </head>
 <body>
-    <h3>There is the ranking of the best players</h3>
+    <h2>There is the ranking of the best players</h2>
+    <h3>The best players of the game</h3>
     <?php
-        require("../model/BdPdoConnection.php");
-        require("../model/PlayerDAO.php");
-        require("../model/GameDAO.php");
-        require("../model/RoundDAO.php");
-        require("../model/Round1DAO.php");
-        require("../model/Round2DAO.php");
-        require("../model/Round3DAO.php");      
+        displayBestPlayers();
+    ?>
+    <h3>Round1 ranking</h3>
+    <?php
+        displayRound1Rank();
+    ?>
+    <h3>Round2 ranking</h3>
+    <?php
+        displayRound2Rank();
+    ?>
+    <h3>Round3 ranking</h3>
+    <?php
+        displayRound3Rank();
+    ?>
+    <h3>The general ranking</h3>
+    <?php
         
-        $newPlayerDAO = new PlayerDAO(BdPdoConnection::getConnection());
-        $newGameDAO = new GameDAO(BdPdoConnection::getConnection());
-        $newRoundDAO = new RoundDAO(BdPdoConnection::getConnection());
-        $newRound1DAO = new Round1DAO(BdPdoConnection::getConnection());
-        $newRound2DAO = new Round2DAO(BdPdoConnection::getConnection());
-        $newRound3DAO = new Round3DAO(BdPdoConnection::getConnection());
-
-        // get the players
-        $players = $newPlayerDAO->getAllPlayers(); // ok
-
-        // test
-        // echo "Number of players = " . count($players) . "<br>";
-        // echo "<br>Each players (pseudos) : <br>";
-        // for ($i=0;$i<count($players);$i++)
-        // {
-        //     echo $players[$i]["pseudo"] . "<br>";
-        // }
-        
-        // $gameId = $newGameDAO->selectGame($players[0]["id"])[0]["MAX(id)"];
-        // echo "<br>Game id = " . $gameId . "<br>";
-        // end
-
-        $tableHtml = "<table><thead><tr>";
-        $headers = ["TOP", "USER NAME", "TOTAL TIME", "TOTAL POINTS /32", "ROUND1 : TIME", "ROUND1 : TRIES", "ROUND2 : TIME", "ROUND2 : WORDS FOUND /14", "ROUND3 : TIME", "ROUND3 : RIGHT ANSWERS"];
-
-        // add the headers
-        for ($i = 0; $i < count($headers); $i++) {
-            $tableHtml .= "<th>" . strval($headers[$i]) . "</th>";
-        }
-
-        $tableHtml .= "</tr></thead><tbody>";
-
-        for ($i = 0; $i < count($players); $i++) {
-            $tableHtml .= "<tr><td>" . strval($i) . "</td>"; // top
-            $tableHtml .= "<td>" . $players[$i]["pseudo"] . "</td>"; // user name
-            // total time
-            $gameId = $newGameDAO->selectGame($players[$i]["id"])[0]["MAX(id)"];
-            $roundId = $newRoundDAO->selectRound($gameId)[0]["id"];
-            $round1Time = $newRound1DAO->selectRound1($roundId)[0]["timeSpent"];
-            $round2Time = $newRound2DAO->selectRound2($roundId)[0]["timeSpent"];
-            $round3Time = $newRound3DAO->selectRound3($roundId)[0]["timeSpent"];
-            $totalTime = $round1Time + $round2Time + $round3Time;
-
-            $tableHtml .= "<td>" . strval($totalTime) . "</td>";
-
-            // total points
-            $round1Tries = $newRound1DAO->selectRound1($roundId)[0]["triesNb"]; // /15
-            $round2WordsNb = $newRound2DAO->selectRound2($roundId)[0]["wordsNb"]; // /14
-            $round3RightAnswers = $newRound3DAO->selectRound3($roundId)[0]["rightAnswersNb"]; // /3
-            if ($round1Tries > 15)
-            {
-                $round1Tries = 15;
-            }
-            $totalPoints = $round2WordsNb + $round3RightAnswers + (15 - $round1Tries);
-
-            $tableHtml .= "<td>" . strval($totalPoints) . "</td>";
-
-            $tableHtml .= "<td>" . strval($round1Time) . "</td>"; // round1 time
-            $tableHtml .= "<td>" . strval($round1Tries) . "</td>"; // round1 tries
-            $tableHtml .= "<td>" . strval($round2Time) . "</td>"; // round2 time
-            $tableHtml .= "<td>" . strval($round2WordsNb) . "</td>"; // round2 words found
-            $tableHtml .= "<td>" . strval($round3Time) . "</td>"; // round3 time
-            $tableHtml .= "<td>" . strval($round3RightAnswers) . "</td></tr>"; // round3 right answers
-    
-        }
-        $tableHtml .= "</tbody></table>";
-        echo $tableHtml;
+        displayRankTable();
     ?>
     </body>
     </html>
